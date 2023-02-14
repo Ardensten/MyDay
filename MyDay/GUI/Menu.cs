0f598bd33ddf0e4a8376a66e567c57a940c33d22
Jonatan Ardensten <jonatan.ardensten@gmail.com>
2023-02-14 13:46:57 +0100

    Added models

---
+++ MyDay/GUI/Menu.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyDay.GUI
+{
+    internal class Menu
+    {
+        internal static void Start()
+        {
+            Console.WriteLine($"Välkommen!\n" + 
+                "Logga in\n" +
+                "Registrera dig");
+        }
+    }
+}
---
+++ MyDay/Models/Routine.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyDay.Models
+{
+    internal class Routine
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public TimeOnly TimeOfDay { get; set; }
+        public DateOnly Date { get; set; }
+        public List<Activity> Activities { get; set; }
+    }
+}
